b7c1fda5cbe796a7b24de300a63b6ec27d1c7448
Alex Schouls <alex.schouls@valtech.com>
2023-02-23 16:21:35 +0100

    tests(products): added ProductGenerator tests

---
+++ backend/Products/Tests/Generators/ProductGeneratorBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Generator;
+
+namespace Tests.Generators;
+
+internal class ProductGeneratorBuilder
+{
+    private readonly ProductGenerator _generator;
+
+    public ProductGeneratorBuilder()
+    {
+        _generator = new ProductGenerator();
+    }
+
+    public ProductGenerator Build() => _generator;
+}
---
+++ backend/Products/Tests/Generators/ProductGeneratorTests.cs
@@ -0,0 +1,28 @@
+﻿using Domain;
+using FluentAssertions;
+
+namespace Tests.Generators;
+
+public class ProductGeneratorTests
+{
+    [Theory]
+    [InlineData(1)]
+    [InlineData(10)]
+    [InlineData(100)]
+    [InlineData(1000)]
+    [InlineData(500)]
+    [InlineData(123)]
+    public void GenerateProducts_WithAmountToGenerate_ReturnsGeneratedProducts(int amountToGenerate)
+    {
+        // Arrange
+        var productGenerator = new ProductGeneratorBuilder().Build();
+
+        // Act
+        var result = productGenerator.GenerateProducts(amountToGenerate);
+
+        // Assert
+        result.Should().NotBeEmpty();
+        result.Should().HaveCount(amountToGenerate);
+        result.Should().AllBeOfType<Product>();
+    }
+}
