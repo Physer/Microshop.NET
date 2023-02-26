9bb7672b7457fefe316daf27ea312770710571da
Alex Schouls <alex_schouls@live.com>
2023-02-26 12:26:51 +0100

    tests(products): started on Product Repository tests

---
+++ backend/Products/Tests/Repositories/ProductRepositoryBuilder.cs
@@ -0,0 +1,33 @@
+﻿using Domain;
+using NSubstitute;
+using Persistence;
+
+namespace Tests.Repositories;
+
+internal class ProductRepositoryBuilder
+{
+    private readonly IProductMapper _productMapperMock;
+    private readonly List<ProductData> _data;
+
+    public ProductRepositoryBuilder()
+    {
+        _productMapperMock = Substitute.For<IProductMapper>();
+        _data = new List<ProductData>();
+    }
+
+    public ProductRepositoryBuilder WithProductData(IEnumerable<ProductData> data)
+    {
+        _data.AddRange(data);
+
+        return this;
+    }
+
+    public ProductRepositoryBuilder WithMapDatabaseEntryReturns(ProductData input, Product output)
+    {
+        _productMapperMock.MapDatabaseEntryToProduct(input).Returns(output);
+
+        return this;
+    }
+
+    public ProductRepository Build() => new(_productMapperMock, _data);
+}
---
+++ backend/Products/Tests/Repositories/ProductRepositoryTests.cs
@@ -0,0 +1,27 @@
+﻿using AutoFixture.Xunit2;
+using Domain;
+using FluentAssertions;
+using Persistence;
+
+namespace Tests.Repositories;
+
+public class ProductRepositoryTests
+{
+    [Theory, AutoData]
+    public void GetProductById_WithExistingId_ReturnsProduct(ProductData productData, Product output)
+    {
+        // Arrange
+
+        var productRepository = new ProductRepositoryBuilder()
+            .WithProductData(new[] { productData })
+            .WithMapDatabaseEntryReturns(productData, output)
+            .Build();
+
+        // Act
+        var result = productRepository.GetProductById(productData.Id);
+
+        // Assert
+        result.Should().Be(output);
+
+    }
+}
