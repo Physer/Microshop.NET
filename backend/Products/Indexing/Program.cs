1b3ea5d68d1dbfb1354c419036136d2522a9b352
Alex Schouls <alex.schouls@valtech.com>
2023-03-03 08:30:46 +0100

    feat(indexing): added Options for Meilisearch and Products service

--- backend/Products/Indexing/Program.cs
+++ backend/Products/Indexing/Program.cs
@@ -1,4 +1,12 @@
+using Indexing.Options;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+// Options
+builder.Services.Configure<IndexingOptions>(configuration.GetSection(IndexingOptions.ConfigurationEntry));
+builder.Services.Configure<ProductsOptions>(configuration.GetSection(ProductsOptions.ConfigurationEntry));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
