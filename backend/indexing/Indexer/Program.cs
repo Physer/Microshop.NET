0d602c1e48130bac7a05c47c7107557c770814fb
Alex Schouls <alex.schouls@valtech.com>
2023-03-08 13:36:21 +0100

    feat(indexing): added empty Worker service

---
+++ backend/indexing/Indexer/Program.cs
@@ -0,0 +1,10 @@
+using Indexer;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
