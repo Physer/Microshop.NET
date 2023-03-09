2cd965e6885a21a9b1b111cb2a5b16a0ce204543
Alex Schouls <alex.schouls@valtech.com>
2023-03-09 08:52:21 +0100

    feat(indexing): made background service a timed background service

--- backend/Indexing/Indexer/Program.cs
+++ backend/Indexing/Indexer/Program.cs
@@ -3,7 +3,7 @@ using Indexer;
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
-        services.AddHostedService<Worker>();
+        services.AddHostedService<IndexService>();
     })
     .Build();
 
