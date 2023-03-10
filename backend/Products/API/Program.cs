f0ebad1039125c735277c7557a3d4209d6ec4c86
Alex Schouls <alex.schouls@valtech.com>
2023-03-10 15:22:14 +0100

    feat(products): started on MassTransit skeleton

--- backend/Products/API/Program.cs
+++ backend/Products/API/Program.cs
@@ -2,18 +2,33 @@ using API;
 using Application;
 using Application.Interfaces.Generator;
 using Application.Interfaces.Repositories;
+using Application.Options;
+using Consumer;
 using Generator;
 using Mapper;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Options
+var productOptionsSection = builder.Configuration.GetSection(ProductsOptions.ConfigurationEntry);
+var productOptions = productOptionsSection.Get<ProductsOptions>();
+builder.Services.Configure<ProductsOptions>(productOptionsSection);
+
+// MediatR
 builder.Services.AddMediatR(config => config.RegisterServicesFromAssemblyContaining<ApplicationAssemblyPointer>());
 
+// Persistence
 builder.Services.AddSingleton<IRepository, ProductRepository>();
 
+// Generator
 builder.Services.AddTransient<IProductGenerator, ProductGenerator>();
 
+// Mapper
 builder.Services.AddAutoMapper(typeof(ProductProfile));
+
+// Consumer
+builder.Services.RegisterConsumerDependencies(productOptions);
 
 var app = builder.Build();
 
---
+++ backend/Products/Consumer/DependencyRegistrator.cs
@@ -0,0 +1,26 @@
+﻿using Application.Options;
+using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Consumer;
+
+public static class DependencyRegistrator
+{
+    public static void RegisterConsumerDependencies(this IServiceCollection services, ProductsOptions? productsOptions)
+    {
+        if (productsOptions is null)
+            return;
+
+        services.AddMassTransit(busConfigurator =>
+        {
+            busConfigurator.UsingRabbitMq((context, factoryConfigurator) =>
+            {
+                factoryConfigurator.Host(productsOptions.BaseUrl, "/", hostConfigurator => {
+                    hostConfigurator.Username(productsOptions.ManagementUsername);
+                    hostConfigurator.Password(productsOptions.ManagementPassword);
+                });
+                factoryConfigurator.ConfigureEndpoints(context);
+            });
+        });
+    }
+}
