08e038c786ffd46abf4873f8f435c2859c7055e2
Alex Schouls <alex.schouls@valtech.com>
2023-03-24 15:51:53 +0100

    test(indexing): added Unit Tests for Indexing Service

--- backend/Indexing/Tests/Indexing/IndexingServiceBuilder.cs
+++ backend/Indexing/Tests/Indexing/IndexingServiceBuilder.cs
@@ -1,6 +1,41 @@
-﻿namespace Tests.Indexing;
+﻿using Application.Interfaces.Indexing;
+using Application.Interfaces.ProductsClient;
+using Application.Models;
+using AutoMapper;
+using Domain;
+using NSubstitute;
+using Search;
+
+namespace Tests.Indexing;
 
 internal class IndexingServiceBuilder
 {
+    internal readonly IMapper _mapperMock;
+    internal readonly IProductsClient _productsClientMock;
+    internal readonly IIndexingClient _indexingClientMock;
+    internal readonly IMicroshopIndex _indexMock;
 
+    public IndexingServiceBuilder()
+    {
+        _mapperMock = Substitute.For<IMapper>();
+        _productsClientMock = Substitute.For<IProductsClient>();
+        _indexingClientMock = Substitute.For<IIndexingClient>();
+        _indexMock = Substitute.For<IMicroshopIndex>();
+    }
+
+    public IndexingServiceBuilder WithProductsClientReturningProducts(IEnumerable<Product> products)
+    {
+        _productsClientMock.GetProductsAsync(Arg.Any<CancellationToken>()).Returns(products);
+
+        return this;
+    }
+
+    public IndexingServiceBuilder WithMapperMappingToIndexableProducts(IEnumerable<IndexableProduct> indexableProducts)
+    {
+        _mapperMock.Map(Arg.Any<IEnumerable<IndexableProduct>>(), Arg.Any<IEnumerable<Product>>()).Returns(indexableProducts);
+
+        return this;
+    }
+
+    public IndexingService Build() => new(_mapperMock, _productsClientMock, _indexingClientMock, _indexMock);
 }
--- backend/Indexing/Tests/Indexing/IndexingServiceTests.cs
+++ backend/Indexing/Tests/Indexing/IndexingServiceTests.cs
@@ -1,16 +1,36 @@
-﻿using Xunit;
+﻿using Application.Models;
+using AutoFixture;
+using Domain;
+using NSubstitute;
+using Xunit;
 
 namespace Tests.Indexing;
 
 public class IndexingServiceTests
 {
+    private readonly IFixture _fixture;
+
+    public IndexingServiceTests() => _fixture = new Fixture();
+
     [Fact]
-    public async Task IndexProductsAsync_ShouldUseAutomapper()
+    public async Task IndexProductsAsync_ShouldCallDependencies()
     {
         // Arrange
+        var products = _fixture.Create<IEnumerable<Product>>();
+        var indexableProducts = _fixture.Create<IEnumerable<IndexableProduct>>();
+
+        var indexingServiceBuilder = new IndexingServiceBuilder();
+        var indexingService = indexingServiceBuilder
+            .WithProductsClientReturningProducts(products)
+            .WithMapperMappingToIndexableProducts(indexableProducts)
+            .Build();
 
         // Act
+        await indexingService.IndexProductsAsync();
 
         // Assert
+        indexingServiceBuilder._mapperMock.Received(1).Map<IEnumerable<IndexableProduct>>(products);
+        await indexingServiceBuilder._indexingClientMock.Received(1).DeleteAllDocumentsAsync(indexingServiceBuilder._indexMock);
+        await indexingServiceBuilder._indexingClientMock.ReceivedWithAnyArgs(1).AddDocumentsAsync(indexingServiceBuilder._indexMock, indexableProducts);
     }
 }
