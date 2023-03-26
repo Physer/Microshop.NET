f61843223e6ad1751ec9c83ee7b6820f6970077b
Alex Schouls <alex_schouls@live.com>
2023-03-26 11:49:04 +0200

    test(indexing): added IndexingClient tests

---
+++ backend/Indexing/Tests/Builders/IndexingClientBuilder.cs
@@ -0,0 +1,17 @@
+﻿using Application.Interfaces.Indexing;
+using NSubstitute;
+using Search;
+
+namespace Tests.Builders;
+
+internal class IndexingClientBuilder
+{
+    internal readonly IMicroshopIndex _microshopIndex;
+
+    public IndexingClientBuilder()
+    {
+        _microshopIndex = Substitute.For<IMicroshopIndex>();
+    }
+
+    public static IndexingClient Build() => new();
+}
---
+++ backend/Indexing/Tests/IndexingClientTests.cs
@@ -0,0 +1,38 @@
+﻿using AutoFixture.Xunit2;
+using NSubstitute;
+using Tests.Builders;
+using Xunit;
+
+namespace Tests;
+
+public class IndexingClientTests
+{
+    [Fact]
+    public async Task DeletAllDocumentsAsync_CallsIndex()
+    {
+        // Arrange
+        var indexingClientBuilder = new IndexingClientBuilder();
+        var indexingClient = IndexingClientBuilder.Build();
+
+        // Act
+        await indexingClient.DeleteAllDocumentsAsync(indexingClientBuilder._microshopIndex);
+
+        // Assert
+        await indexingClientBuilder._microshopIndex.Received(1).DeleteAllDocumentsAsync();
+    }
+
+    [Theory]
+    [AutoData]
+    public async Task AddDocumentsAsync_CallsIndex(IEnumerable<object> objects)
+    {
+        // Arrange
+        var indexingClientBuilder = new IndexingClientBuilder();
+        var indexingClient = IndexingClientBuilder.Build();
+
+        // Act
+        await indexingClient.AddDocumentsAsync(indexingClientBuilder._microshopIndex, objects);
+
+        // Assert
+        await indexingClientBuilder._microshopIndex.Received(1).AddDocumentsAsync(objects);
+    }
+}
