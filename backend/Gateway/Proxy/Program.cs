93a16f53d621d2ad6e3501b8272f5e73e6f78dd0
Alex Schouls <alex_schouls@live.com>
2023-03-29 15:44:05 +0200

    feat(gateway): added products API configuration

--- backend/Gateway/Proxy/Program.cs
+++ backend/Gateway/Proxy/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("Proxy"));
+
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+app.MapReverseProxy();
 app.Run();
