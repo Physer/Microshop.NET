eb7266bfb5d1a249d953e84836f6a30eec9f2b70
Alex Schouls <alex.schouls@valtech.com>
2023-05-11 16:02:19 +0200

    test(indexing): started on Integration tests

---
+++ backend/Indexing/IntegrationTests/IndexingWorkerTests.cs
@@ -0,0 +1,31 @@
+﻿using Indexer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Xunit;
+
+namespace IntegrationTests;
+
+public class IndexingWorkerTests
+{
+    [Fact]
+    public async Task TestIndexingWorker()
+    {
+        // Arrange
+        var hostBuilder = new HostBuilder().ConfigureServices(services =>
+        {
+            services.AddHostedService<IndexingWorker>();
+        });
+        var host = hostBuilder.Build();
+        await host.StartAsync();
+        var indexingWorker = host.Services.GetRequiredService<IndexingWorker>();
+
+        // Act
+        var cancellationToken = new CancellationToken();
+        await indexingWorker.StartAsync(cancellationToken);
+        Thread.Sleep(5000);
+        await indexingWorker.StopAsync(cancellationToken);
+
+        // Assert
+        // TODO: Check Indexed documents
+    }
+}
