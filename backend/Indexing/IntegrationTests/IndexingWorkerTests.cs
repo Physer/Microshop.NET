001b50cf416a08ee2c07992188f710d44e3708ef
Alex Schouls <alex.schouls@valtech.com>
2023-05-12 08:37:40 +0200

    test(indexing): removed now-obsolete testing code

--- backend/Indexing/IntegrationTests/IndexingWorkerTests.cs
+++ backend/Indexing/IntegrationTests/IndexingWorkerTests.cs
@@ -1,7 +1,4 @@
-﻿using Indexer;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Xunit;
+﻿using Xunit;
 
 namespace IntegrationTests;
 
@@ -11,18 +8,9 @@ public class IndexingWorkerTests
     public async Task TestIndexingWorker()
     {
         // Arrange
-        var hostBuilder = new HostBuilder().ConfigureServices(ServiceConfigurator.ConfigureServices);
-        var host = hostBuilder.Build();
-        await host.StartAsync();
-        var indexingWorker = host.Services.GetRequiredService<IndexingWorker>();
 
         // Act
-        var cancellationToken = new CancellationToken();
-        await indexingWorker.StartAsync(cancellationToken);
-        Thread.Sleep(5000);
-        await indexingWorker.StopAsync(cancellationToken);
 
         // Assert
-        // TODO: Check Indexed documents
     }
 }
