415d91b6c9365174414397c81a9143bad123ac12
Alex Schouls <alex_schouls@live.com>
2023-06-23 10:00:00 +0200

    feat(terraform): added meilisearch and rabbitmq ca

--- infrastructure/terraform/container-apps.tf
+++ infrastructure/terraform/container-apps.tf
@@ -5,13 +5,39 @@ resource "azurerm_container_app_environment" "cae_microshop" {
   log_analytics_workspace_id = azurerm_log_analytics_workspace.log_analytics.id
 }
 
+module "rabbitmq_app" {
+  source                       = "./modules/container-app"
+  application_name             = "rabbitmq"
+  container_app_environment_id = azurerm_container_app_environment.cae_microshop.id
+  image_name                   = "masstransit/rabbitmq:latest"
+  resource_group_name          = azurerm_resource_group.rg_microshop.name
+}
+
+module "meilisearch_app" {
+  source                       = "./modules/container-app"
+  application_name             = "meilisearch"
+  container_app_environment_id = azurerm_container_app_environment.cae_microshop.id
+  image_name                   = "getmeili/meilisearch:latest"
+  resource_group_name          = azurerm_resource_group.rg_microshop.name
+  secrets = tomap({
+    (local.meilisearch_api_key) = random_password.meilisearch_api_key.result
+  })
+  secret_appsettings = {
+    "MEILI_MASTER_KEY" : local.meilisearch_api_key
+  }
+}
+
 module "indexing_app" {
   source                       = "./modules/container-app"
   application_name             = "indexing"
   container_app_environment_id = azurerm_container_app_environment.cae_microshop.id
   image_name                   = "physer/microshop-products:main"
   resource_group_name          = azurerm_resource_group.rg_microshop.name
-  secrets                      = local.secret_map
+  secrets = tomap({
+    (local.rabbitmq_username)   = random_password.rabbitmq_username.result
+    (local.rabbitmq_password)   = random_password.rabbitmq_password.result
+    (local.meilisearch_api_key) = random_password.meilisearch_api_key.result
+  })
   appsettings = {
     "Servicebus__BaseUrl" : "rabbitmq-service",
     "Servicebus__Port" : "5672"
--- infrastructure/terraform/secrets.tf
+++ infrastructure/terraform/secrets.tf
@@ -14,10 +14,4 @@ locals {
   rabbitmq_username   = "rabbitmq-management-username"
   rabbitmq_password   = "rabbitmq-management-password"
   meilisearch_api_key = "meilisearch-api-key"
-
-  secret_map = tomap({
-    (local.rabbitmq_username)   = random_password.rabbitmq_username.result
-    (local.rabbitmq_password)   = random_password.rabbitmq_password.result
-    (local.meilisearch_api_key) = random_password.meilisearch_api_key.result
-  })
 }
