ee903eb23485cef7ad51da5bda683f2f821cbc4c
Alex Schouls <alex_schouls@live.com>
2023-06-23 10:00:11 +0200

    chore(terraform): removed obsolete providers and AD data

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.61.0"
     }
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "~> 2.15.0"
-    }
     random = {
       source  = "hashicorp/random"
       version = "3.5.1"
@@ -18,26 +14,9 @@ terraform {
   }
 }
 
-data "azurerm_client_config" "client_config" {}
-data "azuread_service_principal" "service_principal" {
-  display_name = "schouls_enterprise_mpn"
-}
-data "azuread_user" "alex" {
-  user_principal_name = "alex.schouls@valtech.com"
-}
-
 provider "azurerm" {
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy = true
-    }
-  }
+  features {}
 }
-
-provider "azuread" {
-  tenant_id = data.azurerm_client_config.client_config.tenant_id
-}
-
 resource "azurerm_resource_group" "rg_microshop" {
   name     = "rg-microshop-${var.environment}"
   location = var.location
