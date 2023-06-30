435531972522505840401d382e116837ee45e0c4
Alex Schouls <alex_schouls@live.com>
2023-06-30 10:34:17 +0200

    chore(products): added logging on Products Consumer

--- backend/products/Consumer/ProductsRequestConsumer.cs
+++ backend/products/Consumer/ProductsRequestConsumer.cs
@@ -2,6 +2,7 @@
 using Application.Models;
 using AutoMapper;
 using MassTransit;
+using Microsoft.Extensions.Logging;
 using ProductsClient.Contracts;
 
 namespace Consumer;
@@ -10,18 +11,23 @@ public class ProductsRequestConsumer : IConsumer<GetProductsRequest>
 {
     private readonly IMapper _mapper;
     private readonly IRepository _repository;
+    private readonly ILogger<ProductsRequestConsumer> _logger;
 
     public ProductsRequestConsumer(IMapper mapper,
-        IRepository repository)
+        IRepository repository,
+        ILogger<ProductsRequestConsumer> logger)
     {
         _mapper = mapper;
         _repository = repository;
+        _logger = logger;
     }
 
     public async Task Consume(ConsumeContext<GetProductsRequest> context)
     {
+        _logger.LogInformation("Received request {id} for retrieving products", context.RequestId);
         var products = _repository.GetProducts();
         var productResponses = _mapper.Map<IEnumerable<ProductResponse>>(products);
         await context.RespondAsync(new GetProductsResponse(productResponses));
+        _logger.LogInformation("Send message {messageId} with {productResponsesAmount} products", context.MessageId, productResponses.Count());
     }
 }
