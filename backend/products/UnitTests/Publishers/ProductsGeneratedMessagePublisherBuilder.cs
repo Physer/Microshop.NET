289018b7414695cb56f836f39ed49ec678ae4ba4
Alex Schouls <alex_schouls@live.com>
2023-07-03 15:06:49 +0200

    test(products): added unit test for ProductsGeneratedMessagePublisher

---
+++ backend/products/UnitTests/Publishers/ProductsGeneratedMessagePublisherBuilder.cs
@@ -0,0 +1,20 @@
+﻿using MassTransit;
+using Messaging.Publishers;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+
+namespace UnitTests.Publishers;
+
+internal class ProductsGeneratedMessagePublisherBuilder
+{
+    internal IPublishEndpoint PublishEndpoint;
+    internal ILogger<ProductsGeneratedMessagePublisher> Logger;
+
+    public ProductsGeneratedMessagePublisherBuilder()
+    {
+        PublishEndpoint = Substitute.For<IPublishEndpoint>();
+        Logger = Substitute.For<ILogger<ProductsGeneratedMessagePublisher>>();
+    }
+
+    public ProductsGeneratedMessagePublisher Build() => new(PublishEndpoint, Logger);
+}
---
+++ backend/products/UnitTests/Publishers/ProductsGeneratedMessagePublisherTests.cs
@@ -0,0 +1,24 @@
+﻿using Domain;
+using Messaging.Messages;
+using NSubstitute;
+using Xunit;
+
+namespace UnitTests.Publishers;
+
+public class ProductsGeneratedMessagePublisherTests
+{
+    [Fact]
+    public async Task PublishMessage_CallsDependencies()
+    {
+        // Arrange
+        var productsGeneratedMessagePublisherBuilder = new ProductsGeneratedMessagePublisherBuilder();
+        var productsGeneratedMessagePublisher = productsGeneratedMessagePublisherBuilder.Build();
+
+        // Act
+        await productsGeneratedMessagePublisher.PublishMessage(Enumerable.Empty<Product>());
+
+        // Assert
+        productsGeneratedMessagePublisherBuilder.Logger.Received(1);
+        await productsGeneratedMessagePublisherBuilder.PublishEndpoint.Received(1).Publish(new ProductsGenerated());
+    }
+}
