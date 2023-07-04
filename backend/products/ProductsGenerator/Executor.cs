130db497ea07de6db14ff3ce444c8ddfdcf08773
Alex Schouls <alex.schouls@valtech.com>
2023-07-04 10:38:57 +0200

    feat(products): separating logic from main class and preparing for tests

---
+++ backend/products/ProductsGenerator/Executor.cs
@@ -0,0 +1,25 @@
+﻿using Application.Interfaces.Generator;
+using Application.Interfaces.Messaging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace ProductsGenerator;
+
+internal static class Executor
+{
+    public static async Task GenerateAsync(IHost host)
+    {
+        var amountOfProductsToGenerate = 500;
+        var productGenerator = host.Services.GetRequiredService<IProductGenerator>();
+        var logger = host.Services.GetRequiredService<ILogger<Program>>();
+
+        logger.LogInformation("Generating {amountOfProductsToGenerate} fake products", amountOfProductsToGenerate);
+        var products = productGenerator.GenerateProducts(amountOfProductsToGenerate);
+
+        logger.LogInformation("Sending event that all products have been generated");
+        var messagePublisher = host.Services.GetRequiredService<IProductsGeneratedMessagePublisher>();
+        var publishedMessageId = await messagePublisher.PublishMessage(products);
+        logger.LogInformation("Sent message {messageId}", publishedMessageId);
+    }
+}
--- backend/products/ProductsGenerator/Program.cs
+++ backend/products/ProductsGenerator/Program.cs
@@ -1,24 +1,15 @@
-﻿using Application.Interfaces.Generator;
-using Application.Interfaces.Messaging;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using ProductsGenerator;
+﻿using Microsoft.Extensions.Hosting;
 
-var builder = Host.CreateDefaultBuilder(args).ConfigureServices(ServiceConfigurator.ConfigureServices);
-using var host = builder.Build();
-await host.StartAsync();
+namespace ProductsGenerator;
 
-var amountOfProductsToGenerate = 500;
-var productGenerator = host.Services.GetRequiredService<IProductGenerator>();
-var logger = host.Services.GetRequiredService<ILogger<Program>>();
-
-logger.LogInformation("Generating {amountOfProductsToGenerate} fake products", amountOfProductsToGenerate);
-var products = productGenerator.GenerateProducts(amountOfProductsToGenerate);
-
-logger.LogInformation("Sending event that all products have been generated");
-var messagePublisher = host.Services.GetRequiredService<IProductsGeneratedMessagePublisher>();
-var publishedMessageId = await messagePublisher.PublishMessage(products);
-logger.LogInformation("Sent message {messageId}", publishedMessageId);
-
-await host.StopAsync();
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = Host.CreateDefaultBuilder(args).ConfigureServices(ServiceConfigurator.ConfigureServices);
+        using var host = builder.Build();
+        await host.StartAsync();
+        await Executor.GenerateAsync(host);
+        await host.StopAsync();
+    }
+}
