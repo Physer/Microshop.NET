e741c64f90bfc85eefec50a436fef0018d693b1e
Alex Schouls <alex_schouls@live.com>
2023-07-10 10:55:56 +0200

    feat(gateway): reverting startup/program change

--- backend/gateway/API/Program.cs
+++ backend/gateway/API/Program.cs
@@ -1,8 +1,6 @@
-using API;
-
 var builder = WebApplication.CreateBuilder(args);
-var startup = new Startup();
-startup.ConfigureServices(builder.Services);
 var app = builder.Build();
-startup.Configure(app, app.Environment);
+
+app.MapGet("/", () => "Hello World!");
+
 app.Run();
