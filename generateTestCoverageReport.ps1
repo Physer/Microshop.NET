9300954a886ca6f7bffa669ec7157cbbd20b159a
Alex Schouls <alex_schouls@live.com>
2023-07-11 12:23:39 +0200

    feat(repo): added Powershell script to quickly generate test coverage reports

---
+++ generateTestCoverageReport.ps1
@@ -0,0 +1,21 @@
+$backendServiceName = $Args[0]
+if (!$backendServiceName) {
+  Write-Error 'Please provide a back-end service name as argument'
+  exit
+}
+
+$backendServicePath = "./backend/$($backendServiceName)"
+Push-Location -Path $backendServicePath
+
+$coveragePath = "./coverage"
+if (Test-Path $coveragePath) {
+  Remove-Item -Path $coveragePath -Force -Recurse
+}
+
+dotnet test -c Release --collect:"XPlat Code Coverage" --results-directory $coveragePath
+dotnet-coverage merge "$($coveragePath)/*.cobertura.xml" --recursive --output "$($coveragePath)/coverage.merged.xml" --output-format cobertura
+reportgenerator -reports:"$($coveragePath)/coverage.merged.xml" -targetdir:"$($coveragePath)/report" -reporttypes:Html
+
+Invoke-Expression "$($coveragePath)/report/index.html"
+
+Pop-Location
