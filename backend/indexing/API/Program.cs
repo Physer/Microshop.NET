f6fabe4615529d8b7973d33b92daec9d9699b9fb
Alex Schouls <alex_schouls@live.com>
2023-07-14 10:22:04 +0200

    feat(indexing): added API project instead of Console App

---
+++ backend/indexing/API/Program.cs
@@ -0,0 +1,6 @@
+using API;
+
+var builder = WebApplication.CreateBuilder(args);
+ServiceConfigurator.ConfigureServices(builder.Configuration, builder.Services);
+var app = builder.Build();
+app.Run();
---
+++ backend/indexing/API/ServiceConfigurator.cs
@@ -0,0 +1,30 @@
+﻿using Application.Options;
+using Mapper;
+using Messaging;
+using Search;
+
+namespace API;
+
+public static class ServiceConfigurator
+{
+    public static void ConfigureServices(IConfiguration configuration, IServiceCollection services)
+    {
+        // Options
+        var servicebusOptionsSection = configuration.GetSection(ServicebusOptions.ConfigurationEntry);
+        var servicebusOptions = servicebusOptionsSection.Get<ServicebusOptions>();
+        services.Configure<ServicebusOptions>(servicebusOptionsSection);
+
+        var indexingOptionsSection = configuration.GetSection(IndexingOptions.ConfigurationEntry);
+        var indexingOptions = indexingOptionsSection.Get<IndexingOptions>();
+        services.Configure<IndexingOptions>(indexingOptionsSection);
+
+        // Messaging
+        services.RegisterMessagingDependencies(servicebusOptions);
+
+        // Indexing
+        services.RegisterSearchDependencies(indexingOptions);
+
+        // Automapper
+        services.RegisterMapperDependencies();
+    }
+}
