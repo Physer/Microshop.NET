5088003da0a12b649cb960bafab9ce565a045b76
Alex Schouls <alex_schouls@live.com>
2023-07-16 17:39:53 +0200

    chore(products): removed obsolete  code from custom exception

--- backend/products/Application/Exceptions/MessagePublishingException.cs
+++ backend/products/Application/Exceptions/MessagePublishingException.cs
@@ -1,22 +1,6 @@
-﻿using System.Runtime.Serialization;
-
-namespace Application.Exceptions;
+﻿namespace Application.Exceptions;
 
 public class MessagePublishingException : Exception
 {
-    public MessagePublishingException()
-    {
-    }
-
-    public MessagePublishingException(string? message) : base(message)
-    {
-    }
-
-    public MessagePublishingException(string? message, Exception? innerException) : base(message, innerException)
-    {
-    }
-
-    protected MessagePublishingException(SerializationInfo info, StreamingContext context) : base(info, context)
-    {
-    }
+    public MessagePublishingException(string? message) : base(message) { }
 }
