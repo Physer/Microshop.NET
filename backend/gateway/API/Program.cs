8d4ecf93e3f18c88baf56d0d19f9355eebfaa938
Alex Schouls <alex_schouls@live.com>
2023-07-20 14:07:09 +0200

    feat(gateway): moving back to API for consistency

---
+++ backend/gateway/API/Program.cs
@@ -0,0 +1,17 @@
+using Application.Options;
+using Messaging;
+using Service;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Options
+var servicebusOptionsSection = builder.Configuration.GetSection(ServicebusOptions.ConfigurationEntry);
+var servicebusOptions = servicebusOptionsSection.Get<ServicebusOptions>();
+builder.Services.Configure<ServicebusOptions>(servicebusOptionsSection);
+
+// Messaging
+builder.Services.RegisterMessagingDependencies(servicebusOptions); var app = builder.Build();
+
+app.MapPost("/products", Endpoints.GenerateProducts);
+
+app.Run();
