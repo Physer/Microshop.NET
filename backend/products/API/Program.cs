a3bbe4f753d05ec915d27a5e3b47aa505da0484c
Alex Schouls <alex_schouls@live.com>
2023-07-20 13:39:11 +0200

    feat(products): swapped API project for new Service project

--- backend/products/API/Program.cs
+++
@@ -1,9 +0,0 @@
-using API;
-
-var builder = WebApplication.CreateBuilder(args);
-ServiceConfigurator.ConfigureServices(builder.Configuration, builder.Services);
-var app = builder.Build();
-
-app.MapPost("/products", Endpoints.GenerateProducts);
-
-app.Run();
