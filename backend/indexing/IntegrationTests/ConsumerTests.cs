308720b907f46f488eb0169746d3ffde3787a5f2
Alex Schouls <alex_schouls@live.com>
2023-07-21 16:18:36 +0200

    test(indexing): simplified consumer integration tests

--- backend/indexing/IntegrationTests/ConsumerTests.cs
+++ backend/indexing/IntegrationTests/ConsumerTests.cs
@@ -1,97 +1,25 @@
-using Application.Configuration;
-using Application.Options;
 using AutoFixture.Xunit2;
 using Domain;
-using DotNet.Testcontainers.Builders;
-using DotNet.Testcontainers.Containers;
 using FluentAssertions;
-using IntegrationTests.Configuration;
 using MassTransit;
 using MassTransit.Testing;
-using Messaging;
 using Messaging.Messages;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.Extensions.Configuration;
 using Xunit;
 
 namespace IntegrationTests;
 
-public class ConsumerTests : IAsyncLifetime
+public class ConsumerTests : IClassFixture<ConsumerTestsFixture>
 {
-    private IContainer? _meilisearchContainer;
-    private ServicebusOptions? _servicebusOptions;
-    private IndexingOptions? _indexingOptions;
+    private readonly ConsumerTestsFixture _fixture;
 
-    public Task DisposeAsync() => Task.CompletedTask;
-
-    public async Task InitializeAsync()
-    {
-        var rabbitMqConfiguration = new RabbitMqContainerConfiguration();
-        var rabbitMqContainer = new ContainerBuilder()
-            .WithImage(rabbitMqConfiguration.ImageName)
-            .WithEnvironment(rabbitMqConfiguration.EnvironmentVariables)
-            .WithPortBinding(RabbitMqContainerConfiguration.Port, true)
-            .WithWaitStrategy(Wait
-                .ForUnixContainer()
-                .UntilMessageIsLogged(@"[s|S]erver startup complete")
-                .UntilPortIsAvailable(RabbitMqContainerConfiguration.Port))
-            .Build();
-        await rabbitMqContainer.StartAsync().ConfigureAwait(false);
-
-        var meilisearchConfiguration = new MeilisearchContainerConfiguration();
-        _meilisearchContainer = new ContainerBuilder()
-            .WithImage(meilisearchConfiguration.ImageName)
-            .WithEnvironment(meilisearchConfiguration.EnvironmentVariables)
-            .WithPortBinding(MeilisearchContainerConfiguration.Port, true)
-            .WithWaitStrategy(Wait
-                .ForUnixContainer()
-                .UntilPortIsAvailable(MeilisearchContainerConfiguration.Port))
-            .Build();
-        await _meilisearchContainer.StartAsync().ConfigureAwait(false);
-
-        var rabbitMqContainerPort = rabbitMqContainer.GetMappedPublicPort(RabbitMqContainerConfiguration.Port);
-        var meilisearchContainerPort = _meilisearchContainer.GetMappedPublicPort(MeilisearchContainerConfiguration.Port);
-
-        _servicebusOptions = new()
-        {
-            BaseUrl = RabbitMqContainerConfiguration.Hostname,
-            ManagementUsername = RabbitMqContainerConfiguration.Username,
-            ManagementPassword = RabbitMqContainerConfiguration.Password,
-            Port = rabbitMqContainerPort
-        };
-        _indexingOptions = new()
-        {
-            BaseUrl = $"http://localhost:{meilisearchContainerPort}/",
-            ApiKey = MeilisearchContainerConfiguration.ApiKey,
-            IndexingIntervalInSeconds = MeilisearchContainerConfiguration.IntervalInSeconds
-        };
-    }
+    public ConsumerTests(ConsumerTestsFixture fixture) => _fixture = fixture;
 
     [Theory]
     [AutoData]
-    public async void ReceivesMessage_ProductsGenerated_IndexesProducts(IEnumerable<Product> products)
+    public async void ProductsGeneratedConsumer_ConsumesMessage(IEnumerable<Product> products)
     {
-        // Arrange
-        Dictionary<string, string?> configuration = new()
-        {
-            ["Indexing:BaseUrl"] = _indexingOptions!.BaseUrl,
-            ["Indexing:ApiKey"] = _indexingOptions!.ApiKey,
-            ["Indexing:IndexingIntervalInSeconds"] = _indexingOptions!.IndexingIntervalInSeconds.ToString(),
-            ["Servicebus:BaseUrl"] = _servicebusOptions!.BaseUrl,
-            ["Servicebus:ManagementUsername"] = _servicebusOptions!.ManagementUsername,
-            ["Servicebus:ManagementPassword"] = _servicebusOptions!.ManagementPassword,
-            ["Servicebus:Port"] = _servicebusOptions!.Port.ToString()
-        };
-        TestConfiguration.Create(builder => builder.AddInMemoryCollection(configuration));
-        var application = new WebApplicationFactory<Program>()
-            .WithWebHostBuilder(builder =>
-            {
-                builder.ConfigureServices(services =>
-                {
-                    services.AddMassTransitTestHarness(cfg => cfg.ConfigureBusRegistration(_servicebusOptions));
-                });
-            });
-        var testHarness = application.Services.GetTestHarness();
+        // Arranges
+        var testHarness = _fixture.TestHarness;
 
         // Act
         await testHarness.Start();
@@ -99,9 +27,22 @@ public class ConsumerTests : IAsyncLifetime
         await testHarness.Stop();
 
         // Assert
-        var containerLogs = await _meilisearchContainer!.GetLogsAsync();
-        containerLogs.Should().NotBeNull();
-        containerLogs.Stderr.Should().Contain("indexed_documents:");
-        containerLogs.Stderr.Should().NotContain("indexed_documents: 0");
+        (await testHarness.Consumed.Any<ProductsGenerated>()).Should().BeTrue();
+    }
+
+    [Theory]
+    [AutoData]
+    public async void PricesGeneratedConsumer_ConsumesMessage(IEnumerable<Price> prices)
+    {
+        // Arranges
+        var testHarness = _fixture.TestHarness;
+
+        // Act
+        await testHarness.Start();
+        await testHarness.Bus.Publish<PricesGenerated>(new(prices));
+        await testHarness.Stop();
+
+        // Assert
+        (await testHarness.Consumed.Any<PricesGenerated>()).Should().BeTrue();
     }
 }
---
+++ backend/indexing/IntegrationTests/ConsumerTestsFixture.cs
@@ -0,0 +1,20 @@
+﻿using MassTransit;
+using MassTransit.Testing;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace IntegrationTests;
+
+public class ConsumerTestsFixture
+{
+    public ITestHarness TestHarness { get; private set; }
+
+    public ConsumerTestsFixture()
+    {
+        TestHarness = new WebApplicationFactory<Program>()
+            .WithWebHostBuilder(builder => builder
+                .ConfigureServices(services => services
+                    .AddMassTransitTestHarness()))
+            .Services
+            .GetTestHarness();
+    }
+}
