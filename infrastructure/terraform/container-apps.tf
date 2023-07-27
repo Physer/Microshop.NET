5dfea36b2569f7e62ea5084384b8a39529da3706
Alex Schouls <alex_schouls@live.com>
2023-07-27 10:26:53 +0200

    fix(terraform): updated Authentication service ingress settings

--- infrastructure/terraform/container-apps.tf
+++ infrastructure/terraform/container-apps.tf
@@ -84,6 +84,9 @@ module "authentication_database" {
   application_name             = "authentication-db"
   container_app_environment_id = azurerm_container_app_environment.cae_microshop.id
   image_name                   = "postgres:latest"
+  port                         = 5432
+  transport                    = "tcp"
+  ingress_enabled              = true
   resource_group_id            = azurerm_resource_group.rg_microshop.id
   secrets                      = local.authentication_database_secrets
   appsettings                  = local.authentication_database_appsettings
@@ -96,6 +99,7 @@ module "authentication_core" {
   container_app_environment_id = azurerm_container_app_environment.cae_microshop.id
   image_name                   = "registry.supertokens.io/supertokens/supertokens-postgresql:6.0"
   resource_group_id            = azurerm_resource_group.rg_microshop.id
+  port                         = 3567
   ingress_enabled              = true
   secrets                      = local.authentication_core_secrets
   appsettings                  = local.authentication_core_appsettings
