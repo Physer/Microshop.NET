507fedf78a4cec8c1009da18d459b81df9192fa9
Alex Schouls <alex_schouls@live.com>
2023-08-08 15:37:11 +0200

    chore(repo): added dotnet tools installations to PS script

--- generateTestCoverageReport.ps1
+++ generateTestCoverageReport.ps1
@@ -12,6 +12,9 @@ if (Test-Path $coveragePath) {
   Remove-Item -Path $coveragePath -Force -Recurse
 }
 
+dotnet tool install --global dotnet-coverage
+dotnet tool install --global dotnet-reportgenerator-globaltool
+
 dotnet test -c Release --collect:"XPlat Code Coverage" --results-directory $coveragePath
 dotnet-coverage merge "$($coveragePath)/*.cobertura.xml" --recursive --output "$($coveragePath)/coverage.merged.xml" --output-format cobertura
 reportgenerator -reports:"$($coveragePath)/coverage.merged.xml" -targetdir:"$($coveragePath)/report" -reporttypes:Html
