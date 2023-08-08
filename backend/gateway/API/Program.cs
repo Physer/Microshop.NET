75e354c5eeabcdb402b4e89c3ccef8a6f543645b
Alex Schouls <alex_schouls@live.com>
2023-08-08 08:31:01 +0200

    feat(gateway): added auth options

--- backend/gateway/API/Program.cs
+++ backend/gateway/API/Program.cs
@@ -1,6 +1,7 @@
 using Application.Options;
 using Messaging;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using Service;
 
@@ -11,8 +12,9 @@ var servicebusOptionsSection = builder.Configuration.GetSection(ServicebusOption
 var servicebusOptions = servicebusOptionsSection.Get<ServicebusOptions>();
 builder.Services.Configure<ServicebusOptions>(servicebusOptionsSection);
 
-var reverseProxyOptionsSection = builder.Configuration.GetSection(ProxyOptions.ConfigurationEntry);
-var reverseProxyOptions = reverseProxyOptionsSection.Get<ProxyOptions>();
+var authenticationOptions = builder.Configuration.GetSection(AuthenticationOptions.ConfigurationEntry).Get<AuthenticationOptions>();
+if (authenticationOptions is null)
+    throw new ArgumentNullException(nameof(authenticationOptions), "Invalid authentication options");
 
 // Messaging
 builder.Services.RegisterMessagingDependencies(servicebusOptions);
@@ -21,15 +23,16 @@ builder.Services.RegisterMessagingDependencies(servicebusOptions);
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, async c =>
 {
     using var client = new HttpClient();
-    client.BaseAddress = new Uri(reverseProxyOptions?.Clusters.Authentication.Destinations.Authentication.Address ?? string.Empty);
-    var jwksResponseMessage = await client.GetAsync("/auth/jwt/jwks.json");
+    client.BaseAddress = new Uri(authenticationOptions.BaseUrl);
+    var jwksResponseMessage = await client.GetAsync(authenticationOptions.RelativeJwksEndpoint);
     var jwksJson = await jwksResponseMessage.Content.ReadAsStringAsync();
     var jwks = new JsonWebKeySet(jwksJson) ?? throw new UnauthorizedAccessException("Unable to validate the JWKS");
 
     c.TokenValidationParameters = new TokenValidationParameters
     {
         IssuerSigningKeys = jwks.Keys,
-        ValidateAudience = false
+        ValidateAudience = false,
+        ValidateIssuer = false
     };
 });
 builder.Services.AddAuthorization();
--- backend/gateway/Application/Options/ProxyOptions.cs
+++ backend/gateway/Application/Options/ProxyOptions.cs
@@ -3,25 +3,4 @@
 public class ProxyOptions
 {
     public const string ConfigurationEntry = "ReverseProxy";
-    public required ProxyClusterElement Clusters { get; init; }
-}
-
-public class ProxyClusterElement
-{
-    public required ProxyClusterDestinationElement Authentication { get; init; }
-}
-
-public class ProxyClusterDestinationElement
-{
-    public required ProxyClusterDestination Destinations { get; init; }
-}
-
-public class ProxyClusterDestination
-{
-    public required ProxyClusterDestinationValue Authentication { get; init; }
-}
-
-public class ProxyClusterDestinationValue
-{
-    public required string Address { get; init; }
 }
