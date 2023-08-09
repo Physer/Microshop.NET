d0f445d9380eb84fb947f971294e862c8697d1f0
Alex Schouls <alex_schouls@live.com>
2023-08-09 14:32:37 +0200

    feat(terraform): implemented deployment for Admin UI

---
+++ infrastructure/terraform/admin-ui.tf
@@ -0,0 +1,8 @@
+module "admin_ui_app" {
+  source                       = "./modules/container-app"
+  application_name             = "admin-ui"
+  container_app_environment_id = azurerm_container_app_environment.cae_microshop.id
+  image_name                   = "physer/microshop-admin-ui:main"
+  resource_group_id            = azurerm_resource_group.rg_microshop.id
+  revision_suffix              = random_pet.revision_suffix.id
+}
