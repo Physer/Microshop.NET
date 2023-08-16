b30a659b20de661e691f63b2db28b3cab0af6c1c
Alex Schouls <alex_schouls@live.com>
2023-08-16 15:47:38 +0200

    feat(terraform): upgrading Providers

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -2,19 +2,19 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.61.0"
+      version = ">= 3.61.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.5.1"
+      version = ">= 3.5.1"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 4.0"
+      version = ">= 4.0"
     }
     azapi = {
       source  = "Azure/azapi"
-      version = "~> 1.7.0"
+      version = ">= 1.7.0"
     }
   }
   backend "azurerm" {
