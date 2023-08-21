d0bad3fe3d7c15d10b817342d41ad86a98635662
Alex Schouls <alex_schouls@live.com>
2023-08-21 11:55:04 +0200

    feat(terraform): implemented dns proxy provisioining

---
+++ infrastructure/terraform/dns-proxy/variables.tf
@@ -0,0 +1,25 @@
+variable "environment" {
+  type = string
+}
+
+variable "location" {
+  type    = string
+  default = "West Europe"
+}
+
+variable "cloudflare_api_token" {
+  description = "The Cloudflare API token for interfacing with the DNS zone API"
+  sensitive   = true
+  type        = string
+}
+
+variable "cloudflare_zone_id" {
+  description = "The Zone ID for the Cloudflare account"
+  sensitive   = true
+  type        = string
+}
+
+variable "application_names" {
+  description = "A set of application names to create DNS records for"
+  type        = set(string)
+}
