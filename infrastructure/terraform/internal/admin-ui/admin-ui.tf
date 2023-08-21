b964838601e518428739ee6d6c4fd36a56dc5337
Alex Schouls <alex_schouls@live.com>
2023-08-21 09:50:25 +0200

    feat(terraform): implementing admin-ui

--- infrastructure/terraform/internal/admin-ui/admin-ui.tf
+++ infrastructure/terraform/internal/admin-ui/admin-ui.tf
@@ -1,9 +1,18 @@
+data "azurerm_resource_group" "rg_microshop" {
+  name = "rg-microshop-${var.environment}"
+}
+
+data "azurerm_container_app_environment" "cae_microshop" {
+  name                = "cae-${var.environment}"
+  resource_group_name = "rg-microshop-${var.environment}"
+}
+
 module "admin_ui_app" {
   source                       = "../../modules/container-app"
   application_name             = "admin-ui"
-  container_app_environment_id = azurerm_container_app_environment.cae_microshop.id
+  container_app_environment_id = data.azurerm_container_app_environment.cae_microshop.id
   image_name                   = "physer/microshop-admin-ui:main"
   ingress_enabled              = true
-  resource_group_id            = azurerm_resource_group.rg_microshop.id
-  location                     = azurerm_resource_group.rg_microshop.location
+  resource_group_id            = data.azurerm_resource_group.rg_microshop.id
+  location                     = data.azurerm_resource_group.rg_microshop.location
 }
