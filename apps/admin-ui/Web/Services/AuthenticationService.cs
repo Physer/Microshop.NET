6ac8fefe338244114c0536230866c32bf89aa569
Alex Schouls <alex_schouls@live.com>
2023-08-24 13:48:45 +0200

    fix(admin-ui): fixed log in flow

--- apps/admin-ui/Web/Services/AuthenticationService.cs
+++ apps/admin-ui/Web/Services/AuthenticationService.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Authentication;
 using Blazored.SessionStorage;
+using Microsoft.AspNetCore.Components;
 
 namespace Web.Services;
 
@@ -7,17 +8,21 @@ public class AuthenticationService
 {
     private readonly ISessionStorageService _sessionStorageService;
     private readonly IAuthenticationClient _authenticationClient;
+    private readonly NavigationManager _navigationManager;
 
     private const string _tokenStorageKey = "msJwt";
 
     public AuthenticationService(ISessionStorageService sessionStorageService,
-        IAuthenticationClient authenticationClient)
+        IAuthenticationClient authenticationClient,
+        NavigationManager navigationManager)
     {
         _sessionStorageService = sessionStorageService;
         _authenticationClient = authenticationClient;
+        _navigationManager = navigationManager;
     }
 
     public async Task<string> GetTokenAsync() => await _sessionStorageService.GetItemAsStringAsync(_tokenStorageKey);
+
     public async Task SignInAsync(string username, string password)
     {
         var signInResult = await _authenticationClient.SignInAsync(username, password);
@@ -25,5 +30,6 @@ public class AuthenticationService
             throw new UnauthorizedAccessException();
 
         await _sessionStorageService.SetItemAsStringAsync(_tokenStorageKey, signInResult.Token);
+        _navigationManager.NavigateTo("/");
     }
 }
