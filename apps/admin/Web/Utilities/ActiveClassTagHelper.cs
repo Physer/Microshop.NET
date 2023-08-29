c364c75555e8ea69f85efd81ffcd38ec93996d6e
Alex Schouls <alex_schouls@live.com>
2023-08-29 13:51:01 +0200

    feat(admin): added support for toggling active class on anchors

---
+++ apps/admin/Web/Utilities/ActiveClassTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Web.Utilities;
+
+[HtmlTargetElement(Attributes = "active-link-support")]
+public class ActiveClassTagHelper : AnchorTagHelper
+{
+    public ActiveClassTagHelper(IHtmlGenerator generator) : base(generator) { }
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        if (ViewContext?.RouteData?.Values["page"]?.Equals(Page) == false)
+            return;
+
+        var existingClasses = output.Attributes["class"].Value.ToString();
+        output.Attributes.RemoveAll("class");
+        output.Attributes.Add("class", $"{existingClasses} active");
+    }
+}
