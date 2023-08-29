8e008d0aa0f54684e1491506e9e25b2696a19332
Alex Schouls <alex_schouls@live.com>
2023-08-29 09:04:35 +0200

    chore(admin): cleaned up unnecessary code

--- apps/admin/Web/Pages/Index.cshtml.cs
+++ apps/admin/Web/Pages/Index.cshtml.cs
@@ -1,20 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Web.Pages
+namespace Web.Pages;
+
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
-    {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
-        public void OnGet()
-        {
-
-        }
-    }
+    public void OnGet() { }
 }
