d4f60e4595fad4db55f0d973f60437d6aedfea4c
Alex Schouls <alex_schouls@live.com>
2023-08-31 14:14:17 +0200

    fix(admin): updated Startup code

--- apps/admin/Web/Program.cs
+++ apps/admin/Web/Program.cs
@@ -1,6 +1,19 @@
+using Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Web.Utilities;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.RegisterAuthenticationDependencies(builder.Configuration);
 builder.Services.AddRazorPages();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+{
+    var signInPath = "/SignIn";
+    options.SlidingExpiration = true;
+    options.AccessDeniedPath = signInPath;
+    options.LoginPath = signInPath;
+    options.LogoutPath = signInPath;
+});
 builder.Services.AddAuthorization(options => options.AddPolicy(AuthorizationDefaults.Administrator, policy => policy.RequireClaim("Role", AuthorizationDefaults.Administrator)));
 
 var app = builder.Build();
@@ -14,6 +27,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseStaticFiles();
 app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapRazorPages();
 app.Run();
