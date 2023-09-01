a8ebc3c686e67dd3bd0c5e3dc0f0028102a625c2
Alex Schouls <alex_schouls@live.com>
2023-09-01 10:16:52 +0200

    fix(terraform): updated Admin appsettings and fixed path

--- infrastructure/terraform/external/admin.tf
+++ infrastructure/terraform/external/admin.tf
@@ -1,13 +1,17 @@
 locals {
   resource_group_name = "rg-microshop-${var.environment}"
+  admin_appsettings = [
+    { name = "Authentication__BaseUrl", value = "https://${module.authentication_service.fqdn}" },
+  ]
 }
 
 module "admin_app" {
-  source                       = "../../modules/container-app"
+  source                       = "../modules/container-app"
   application_name             = "admin"
   container_app_environment_id = data.azurerm_container_app_environment.cae_microshop.id
   image_name                   = "physer/microshop-admin:main"
   ingress_enabled              = true
   resource_group_id            = data.azurerm_resource_group.rg_microshop.id
   location                     = data.azurerm_resource_group.rg_microshop.location
+  appsettings                  = local.admin_appsettings
 }
