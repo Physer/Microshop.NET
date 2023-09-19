0d4ae4d3c70b1129a2a1aba1a618653a06265726
Alex Schouls <alex_schouls@live.com>
2023-09-19 11:59:06 +0200

    fix(admin): fixed typo in User Management

--- apps/admin/UserManagement/DependencyRegistrator.cs
+++ apps/admin/UserManagement/DependencyRegistrator.cs
@@ -12,7 +12,7 @@ public static class DependencyRegistrator
         var userManagementOptionsSection = configuration.GetSection(UserManagementOptions.ConfigurationEntry);
         var userManagementOptions = userManagementOptionsSection.Get<UserManagementOptions>();
         if (userManagementOptions is null)
-            throw new ArgumentNullException(nameof(userManagementOptions), "Invalid authentication options");
+            throw new ArgumentNullException(nameof(userManagementOptions), "Invalid user management options");
 
         services.AddHttpClient<IUserClient, UserClient>(configuration => configuration.BaseAddress = new Uri(userManagementOptions.BaseUrl));
     }
