c4be1dd5956dc015814a60c6ee67e184ed450d13
Alex Schouls <alex_schouls@live.com>
2023-11-02 15:33:03 +0100

    test(admin): finalized UserClient tests with valid use case

--- apps/admin/UnitTests/UserManagement/UserClientTests.cs
+++ apps/admin/UnitTests/UserManagement/UserClientTests.cs
@@ -50,4 +50,26 @@ public class UserClientTests
         exception.Should().BeOfType<MicroshopApiException>();
         exception.Message.Should().BeEquivalentTo(expectedErrorMessage);
     }
+
+    [Fact]
+    public async Task GetUsersAsync_WithValidData_ReturnsUsers()
+    {
+        // Arrange
+        GetUsersResponse validGetUsersResponse = _fixture.Create<GetUsersResponse>();
+        UserRolesResponse validUserRolesResponse = _fixture.Create<UserRolesResponse>();
+        var userClient = new UserClientBuilder()
+            .WithGetUserDataAsyncReturning(validGetUsersResponse)
+            .WithGetUsersInAdminRoleAsyncReturning(validUserRolesResponse)
+            .Build();
+
+        // Act
+        var users = userClient.GetUsersAsync();
+
+        // Assert
+        var userResponseEmails = validGetUsersResponse.Users.Select(usersResponse => usersResponse.User.Email);
+        var userEmails = await users.Select(user => user.EmailAddress).ToListAsync();
+
+        (await users.CountAsync()).Should().Be(validGetUsersResponse.Users.Count());
+        userEmails.Should().BeEquivalentTo(userResponseEmails);
+    }
 }
