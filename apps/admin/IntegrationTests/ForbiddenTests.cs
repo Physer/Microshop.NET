0dc46dbe85ce5006ad3641cf3d357a269aa45edb
Alex Schouls <alex_schouls@live.com>
2023-11-10 16:00:28 +0100

    test(admin): added Forbidden test

---
+++ apps/admin/IntegrationTests/ForbiddenTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using IntegrationTests.Utilities;
+using Xunit;
+
+namespace IntegrationTests;
+
+[Collection(nameof(AuthenticationCollectionFixture))]
+public class ForbiddenTests
+{
+    private readonly AuthenticationFixture _fixture;
+
+    public ForbiddenTests(AuthenticationFixture fixture) => _fixture = fixture;
+
+    [Fact]
+    public async Task ForbiddenPage_ShowsMessage()
+    {
+        // Arrange
+        var expectedErrorMessage = "You do not have administrative rights for Microshop.NET.";
+        var applicationFactory = _fixture.ValidApplicationFactory!;
+        var client = applicationFactory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/forbidden");
+        var content = await HtmlHelpers.GetDocumentAsync(response);
+
+        // Assert
+        content.Body?.InnerHtml.Should().Contain(expectedErrorMessage);
+    }
+}
