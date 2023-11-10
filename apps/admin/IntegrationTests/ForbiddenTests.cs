0060310b275d1436f44b1d6d1373c55851048136
Alex Schouls <alex_schouls@live.com>
2023-11-10 16:26:23 +0100

    test(admin): added signout test on Forbidden page

--- apps/admin/IntegrationTests/ForbiddenTests.cs
+++ apps/admin/IntegrationTests/ForbiddenTests.cs
@@ -1,5 +1,8 @@
-﻿using FluentAssertions;
+﻿using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+using FluentAssertions;
 using IntegrationTests.Utilities;
+using Web.Pages;
 using Xunit;
 
 namespace IntegrationTests;
@@ -8,6 +11,7 @@ namespace IntegrationTests;
 public class ForbiddenTests
 {
     private readonly AuthenticationFixture _fixture;
+    private const string _forbiddenUrl = "/forbidden";
 
     public ForbiddenTests(AuthenticationFixture fixture) => _fixture = fixture;
 
@@ -20,10 +24,28 @@ public class ForbiddenTests
         var client = applicationFactory.CreateClient();
 
         // Act
-        var response = await client.GetAsync("/forbidden");
+        var response = await client.GetAsync(_forbiddenUrl);
         var content = await HtmlHelpers.GetDocumentAsync(response);
 
         // Assert
         content.Body?.InnerHtml.Should().Contain(expectedErrorMessage);
+    }
+
+    [Fact]
+    public async Task ForbiddenPage_OnPost_RedirectsToSignOut()
+    {
+        // Arrange
+        var applicationFactory = _fixture.ValidApplicationFactory!;
+        var client = applicationFactory.CreateClient();
+        var forbiddenPage = await client.GetAsync(_forbiddenUrl);
+        var content = await HtmlHelpers.GetDocumentAsync(forbiddenPage);
+        var form = content.QuerySelector<IHtmlFormElement>("form") ?? throw new Exception("Unable to find the sign in form");
+        var submitButton = content.QuerySelector<IHtmlInputElement>("input[id='signOutButton']") ?? throw new Exception("Unable to find the submit button on the sign in form");
+
+        // Act
+        var response = await client.SendAsync(form, submitButton);
+
+        // Assert
+        response.RequestMessage?.RequestUri?.PathAndQuery.Should().BeEquivalentTo("/signout");
     }
 }
