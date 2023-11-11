476ae4c9a34c3dcdb353239c2866400f4a664a0a
Alex Schouls <alex_schouls@live.com>
2023-11-11 17:31:41 +0100

    test(admin): added Index page tests

---
+++ apps/admin/IntegrationTests/IndexTests.cs
@@ -0,0 +1,76 @@
+﻿using FluentAssertions;
+using IntegrationTests.Utilities;
+using System.Net;
+using Xunit;
+
+namespace IntegrationTests;
+
+[Collection(nameof(AuthenticationCollectionFixture))]
+public class IndexTests
+{
+    private readonly AuthenticationFixture _fixture;
+    private const string _signInUrl = "/signin";
+
+    public IndexTests(AuthenticationFixture fixture) => _fixture = fixture;
+
+    [Theory]
+    [InlineData("/")]
+    [InlineData("/index")]
+    [InlineData("/Index")]
+    public async Task IndexPage_ForAnonymousUser_RedirectsToSignIn(string url)
+    {
+        // Arrange
+        var applicationFactory = _fixture.ValidApplicationFactory!;
+        var client = applicationFactory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync(url);
+
+        // Assert
+        response.Should().NotBeNull();
+        response.RequestMessage?.RequestUri?.PathAndQuery.Should().BeEquivalentTo(_signInUrl);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Theory]
+    [InlineData("/")]
+    [InlineData("/index")]
+    [InlineData("/Index")]
+    public async Task IndexPage_ForNonAdminUser_RedirectsToForbidden(string url)
+    {
+        // Arrange
+        var applicationFactory = _fixture.ValidApplicationFactory!;
+        var client = applicationFactory.CreateClient();
+        _ = await AuthenticationFixture.SendSignInRequestAsync(client, _fixture.ForbiddenUser.Username, _fixture.ForbiddenUser.Password);
+
+        // Act
+        var response = await client.GetAsync(url);
+
+        // Assert
+        response.Should().NotBeNull();
+        response.RequestMessage?.RequestUri?.PathAndQuery.Should().BeEquivalentTo("/forbidden");
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Theory]
+    [InlineData("/")]
+    [InlineData("/index")]
+    [InlineData("/Index")]
+    public async Task IndexPage_ForAdminUser_ShowsIndex(string url)
+    {
+        // Arrange
+        var expectedContent = "Admin portal";
+        var applicationFactory = _fixture.ValidApplicationFactory!;
+        var client = applicationFactory.CreateClient();
+        _ = await AuthenticationFixture.SendSignInRequestAsync(client, _fixture.AdminUser.Username, _fixture.AdminUser.Password);
+
+        // Act
+        var response = await client.GetAsync(url);
+        var document = await HtmlHelpers.GetDocumentAsync(response);
+
+        // Assert
+        response.Should().NotBeNull();
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        document.Body?.InnerHtml.Should().Contain(expectedContent);
+    }
+}
