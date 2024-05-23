0e55eef911c127449b14c3a606b33ff807d1c56a
Alex Schouls <alex_schouls@live.com>
2024-05-23 13:17:36 +0200

    feat(libs): moved InlineWebApplicationFactory to .NET 8 and new structure

--- libs/inlinewebapplicationfactory/Library/InlineWebApplicationFactory.cs
+++ libs/inlinewebapplicationfactory/Library/InlineWebApplicationFactory.cs
@@ -4,11 +4,9 @@ using Microsoft.Extensions.Configuration;
 
 namespace InlineWebApplicationFactory;
 
-public class InlineWebApplicationFactory<T> : WebApplicationFactory<T> where T : class
+public class InlineWebApplicationFactory<T>(IDictionary<string, string?> configuration) : WebApplicationFactory<T> where T : class
 {
-    private readonly IDictionary<string, string?> _configuration;
-
-    public InlineWebApplicationFactory(IDictionary<string, string?> configuration) => _configuration = configuration;
+    private readonly IDictionary<string, string?> _configuration = configuration;
 
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
