c9065f985077a6a3a0a1b0286c6b1bcf5db1e6f9
Alex Schouls <alex_schouls@live.com>
2025-08-04 17:14:13 +0200

    fix(terraform): updated external providers

--- infrastructure/terraform/external/provider.tf
+++ infrastructure/terraform/external/provider.tf
@@ -2,19 +2,19 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 4.34.0"
+      version = "4.38.1"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.7.2"
+      version = "3.7.2"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = ">= 5.6.0"
+      version = "5.8.2"
     }
     azapi = {
       source  = "Azure/azapi"
-      version = ">= 2.4.0"
+      version = "2.5.0"
     }
   }
   backend "azurerm" {
