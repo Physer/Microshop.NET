027068f51b82b0a7bc0276dc0bfa04c527ce59e1
Alex Schouls <alex_schouls@live.com>
2025-08-04 16:57:50 +0200

    chore(terraform): updated variables to no longer have default latest image tags

--- infrastructure/terraform/generic/variables.tf
+++ infrastructure/terraform/generic/variables.tf
@@ -10,11 +10,9 @@ variable "location" {
 variable "rabbitmq_docker_image_version" {
   description = "The docker image version of the RabbitMQ servicebus"
   type        = string
-  default     = "latest"
 }
 
 variable "meilisearch_docker_image_version" {
   description = "The docker image version of the Meilisearch index"
   type        = string
-  default     = "latest"
 }
